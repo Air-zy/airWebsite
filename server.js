9ead6b3c1730795ed784ac07a121aa82f9c20788
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-06-16 07:27:43 +0000

    Your commit message

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// static files from "public" dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+// fallback
 app.use((req, res) => res.send('hi'));
 
 app.listen(PORT, () => {
