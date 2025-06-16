df87b29f778fd8dc4d36576942eb9a5e4c154165
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-06-16 07:37:45 +0000

    serverjsmod

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 3000;
 // static files from "public" dir
 app.use(express.static(path.join(__dirname, 'public')));
 
-// fallback
-app.use((req, res) => res.send('hi'));
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
