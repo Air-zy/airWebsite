4a0461faf9be107fdebb41d6f7aadd8f66056614
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-06-26 22:06:00 +0000

    upd protected

--- src/envDecrypt.js
+++ src/envDecrypt.js
@@ -1,7 +1,7 @@
 const { decrypt } = require('./aes-128_ecb.js');
 
 function envDecrypt(key, ciphertext) {
-  keyBytes = Buffer.from(key, 'hex');
+  keyBytes = Buffer.from(key, 'base64');
   const cipherBuf = Buffer.from(ciphertext, 'base64');
 
   const plainBytes = decrypt(
