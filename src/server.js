93fb009644af7c893db7cc6f508aed91db15a059
air-zy <airzyalt@gmail.com>
2025-06-29 06:34:40 -0600

    more organizationn

--- src/server.js
+++ src/server.js
@@ -22,25 +22,25 @@ app.use(express.json());
 app.use(express.static(PRODUCTION_PUBLIC_DIRECTORY));
 
 // routes 
-app.get('/home',       (req, res) => { return res.redirect('/index.html');      });
-app.get('/c4',         (req, res) => { return res.redirect('/c4/connect4.html');});
-app.get('/avyTos',     (req, res) => { return res.redirect('/avyTOS.html');     });
-app.get('/avyprivacy', (req, res) => { return res.redirect('/avyPrivacy.html'); });
-app.get('/change',     (req, res) => { return res.redirect('/change.html');     });
-app.get('/quad',       (req, res) => { return res.redirect('/quadratic.html');  });
-app.get('/coinsort',   (req, res) => { return res.redirect('/change.html');     });
-app.get('/journal',    (req, res) => { return res.redirect('/journal.html');    });
-app.get('/deepwoken',  (req, res) => { return res.redirect('/deepwoken.html');  });
-app.get('/anime',      (req, res) => { return res.redirect('/anime.html');    });
+app.get('/home',       (req, res) => { return res.redirect('/index.html');            });
+app.get('/c4',         (req, res) => { return res.redirect('/c4/connect4.html');      });
+app.get('/avyTos',     (req, res) => { return res.redirect('/avyTOS.html');           });
+app.get('/avyprivacy', (req, res) => { return res.redirect('/avyPrivacy.html');       });
+app.get('/change',     (req, res) => { return res.redirect('/change.html');           });
+app.get('/quad',       (req, res) => { return res.redirect('/quadratic.html');        });
+app.get('/coinsort',   (req, res) => { return res.redirect('/change.html');           });
+app.get('/journal',    (req, res) => { return res.redirect('/journal/journal.html');  });
+app.get('/deepwoken',  (req, res) => { return res.redirect('/deepwoken.html');        });
+app.get('/anime',      (req, res) => { return res.redirect('/anime.html');            });
 
-app.get('/api/projects', require('./routes/api_projects.js'));
+app.get('/api/projects',            require('./routes/api_projects.js')            );
 
-app.post('/c', require('./routes/c.js'));
-app.post('/validate-me', require('./routes/validate_me.js'));
-app.post('/api/project-edit', require('./routes/api_project_edit.js'));
-app.post('/api/get-anime', require('./routes/get_anime.js'));
-app.post('/api/commit-anime', require('./routes/commit_anime.js'));
-app.post('/api/projects-update', require('./routes/projects_update.js'));
+app.post('/c',                      require('./routes/c.js')                       );
+app.post('/validate-me',            require('./routes/validate_me.js')             );
+app.post('/api/project-edit',       require('./routes/api_project_edit.js')        );
+app.post('/api/get-anime',          require('./routes/get_anime.js')               );
+app.post('/api/commit-anime',       require('./routes/commit_anime.js')            );
+app.post('/api/projects-update',    require('./routes/projects_update.js')         );
 app.use((req, res) => {
   res.status(404).send(`Not found LOL 🥀💔 ${req.method} ${req.originalUrl}`);
 });
