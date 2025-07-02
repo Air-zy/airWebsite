61ac384b9b9d1647c11673c9c94e03e0ab54f043
air-zy <airzyalt@gmail.com>
2025-07-02 04:48:57 -0600

    omggggg

--- src/routes/api_logs.js
+++ src/routes/api_logs.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = require('../config/default.json');
 
 module.exports = async (req, res) => {
-    const logFile = path.join(__dirname, ".", config.log.file);
+    const logFile = path.join(__dirname, "..", config.log.file);
     console.log("logfile 2:",logFile)
     res.setHeader('Content-Type', 'text/plain');
     const readStream = fs.createReadStream(logFile);
