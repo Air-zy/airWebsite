5bef0090026fe1d10009facdd724b4d659daf3e0
air-zy <airzyalt@gmail.com>
2025-07-02 05:18:04 -0600

    finally reqlogger works

--- src/reqLogger.js
+++ src/reqLogger.js
@@ -3,17 +3,19 @@ const path = require('path');
 
 const config = require('./config/default.json');
 const logStream = fs.createWriteStream(path.join(__dirname, config.log.file), { flags: 'a' });
+const { getIP } = require('./routes/ip_utils.js');
 
 module.exports = (req, res, next) => {
   const start = Date.now();
   res.on('finish', () => {
     const duration = Date.now() - start;
     const line = [
-      new Date().toISOString(),
-      req.method,
-      req.originalUrl,
-      res.statusCode,
-      `${duration}ms`
+        getIP(req),
+        new Date().toISOString(),
+        req.method,
+        req.originalUrl,
+        res.statusCode,
+        `${duration}ms`
     ].join(' ') + '\n';
 
     process.stdout.write(line);
