c86ed53c551a09760d8ca96fd84d2c23f41703c9
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-11-11 20:56:50 +0000

    more embed stuff

--- src/routes/rblxapp/routes/rowauser.js
+++ src/routes/rblxapp/routes/rowauser.js
@@ -1,5 +1,5 @@
 const { readFile } = require('fs').promises;
-
+const { getThumb } = require('../rowautils');
 module.exports = async (req, res) => {
   const id = String(req.params.userid || '');
   let loss = '0', wins = '0', elo = '0';
@@ -41,18 +41,33 @@ module.exports = async (req, res) => {
   const truncate = (s, n = 200) => (s.length > n ? s.slice(0, n - 1) + '…' : s);
 
   const title = displayName;
-  const desc = truncate(`${title} — ELO ${elo} • ${wins}W • ${loss}L`, 200);
+  const desc = truncate(`elo: ${elo} \nwins: ${wins} \nlost: ${loss}`, 200);
 
+  let userThumbnail = null
+  try {
+    userThumbnail = await getThumb(id)
+  } catch (err) {
+    console.log("rowautils thumbErr:",err)
+  }
   try {
     let html = await readFile(__dirname + '/res/rowaPlr.html', 'utf8');
-    const meta =
+    let meta =
       `<title>${esc(title)}</title>\n` +
       `<meta property="og:title" content="${esc(title)}">\n` +
       `<meta name="description" content="${esc(desc)}">\n` +
       `<meta property="og:description" content="${esc(desc)}">`;
+
+    if (userThumbnail) {
+        meta += `\n<meta property="og:image" content="${esc(userThumbnail)}">`;
+        meta += `\n<meta name="twitter:image" content="${esc(userThumbnail)}">`;
+        meta += `\n<link rel="image_src" href="${esc(userThumbnail)}">`;
+        meta += `\n<meta property="og:image:alt" content="${esc(title)}">`;
+    }
+
     html = html.replace(/<head(?:\s[^>]*)?>/i, m => m + '\n' + meta);
     res.send(html);
   } catch (err) {
+    console.log("injectile meta on rowaPlr html err:", err)
     res.status(500).end();
   }
 };
---
+++ src/routes/rblxapp/rowautils.js
@@ -0,0 +1,11 @@
+const envDecrypt = require('../../envDecrypt.js');
+const rbxApiKey = envDecrypt(process.env.airKey, process.env.rowaCloudApi)
+
+async function getThumb(userid) {
+    const url = `https://apis.roblox.com/cloud/v2/users/${userid}:generateThumbnail`;
+    const rbxApiRes = await fetch(url, { headers: { 'x-api-key': rbxApiKey }});
+    const data = await rbxApiRes.json();
+    return data.response.imageUri
+};
+
+module.exports = { getThumb };
