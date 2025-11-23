2feca15a6cddf398a73698972454ff9f0869d533
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-11-23 19:59:32 +0000

    api logs fix

--- src/routes/api/api_logs.js
+++ src/routes/api/api_logs.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = require('../../config/default.json');
 
 module.exports = async (req, res) => {
-    const logFile = path.join(__dirname, "..", config.log.file);
+    const logFile = path.join(__dirname, "../..", config.log.file);
     res.setHeader('Content-Type', 'text/plain');
     const readStream = fs.createReadStream(logFile);
     readStream.on('error', err => {
