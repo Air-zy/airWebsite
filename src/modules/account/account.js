ad8066f52f454963dfca2da0e1cf73051b581f03
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-12-12 10:31:36 +0000

    rowa fights html upd

--- src/modules/account/account.js
+++ src/modules/account/account.js
@@ -1,10 +1,41 @@
 const crypto = require('crypto');
 
+const base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+function base64Encode(e) {
+    let s, c = "";
+    for (s = 0; s < e.length; s += 3) {
+        const [a,r=0,n=0] = e.slice(s, s + 3)
+          , t = 3 - (e.length - s)
+          , o = a << 16 | r << 8 | n;
+        c += base64chars[o >> 18 & 63],
+        c += base64chars[o >> 12 & 63],
+        c += t >= 2 ? "=" : base64chars[o >> 6 & 63],
+        c += t >= 1 ? "=" : base64chars[63 & o]
+    }
+    return c
+}
+function base64Decode(e) {
+    const s = [];
+    let c = 0
+      , a = 0;
+    for (const r of e.replace(/=+$/, ""))
+        c = c << 6 | base64chars.indexOf(r),
+        a += 6,
+        a >= 8 && (a -= 8,
+        s.push(c >> a & 255));
+    return s
+}
+
+function generateKey() {
+    const e = crypto.getRandomValues(new Uint8Array(16));
+    base64Encode(e)
+}
+
 class Account {
   constructor(name) {
     this.name = name;
     this.createdAt = Date.now();
-    this.expiresAt = this.createdAt + durationMs;
+    this.aesKey = generateKey();
   }
 }
 
